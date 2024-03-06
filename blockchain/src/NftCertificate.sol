f75777d3142d89650719661715ed4ccdc3a90ff1
Oktavianus Bima Jadiva <93313454+Bimajadivaaa@users.noreply.github.com>
2024-03-06 18:28:49 +0700

    init smart contract

---
+++ blockchain/src/NftCertificate.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+// Created By : Blockchain Developer Interns Team 
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NftCertificate is ERC721, Ownable {
+
+}

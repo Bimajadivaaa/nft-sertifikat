b1f11dc4d8303213e33d746ecc90e18e3251956a
alxxxi <122215088+alxxxi@users.noreply.github.com>
2024-03-07 21:53:13 +0700

    NftCertificate.sol

--- blockchain/src/NftCertificate.sol
+++ blockchain/src/NftCertificate.sol
@@ -8,20 +8,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NftCertificate is ERC721, Ownable {
     uint256 public mintPrice = 100000000000000; //0.0001 ether
-    uint32 public totalSupply;
-    uint32 public maxSupply;
+    uint32 public totalSupply; //initiate total supply
+    uint32 public maxSupply; //initiate total max address can mint
     
-    mapping(address => uint32) public mintedWallets;
+    mapping(address => uint32) public mintedWallets; //Mapping the address to the owners Wallet
     mapping(uint256 => string) public tokenURIs; // Mapping token ID ke hash IPFS metadata
 
+    
     constructor(address initialOwner) payable ERC721('Certificate Mint', 'CERTIFICATE') Ownable(initialOwner){
-        maxSupply = 20;
+        maxSupply = 20; //set maxSupply of the NFTs
     }
 
     function setMaxSupply(uint32 maxSupply_) external onlyOwner {
         maxSupply = maxSupply_;
     }
 
+    //initiate only 1 NFT can be minted per wallet
     function mint(string memory _tokenURI) external payable {
         require(mintedWallets[msg.sender] <1 , 'Exceed max per wallet');
         require(msg.value == mintPrice, 'Wrong Value');
@@ -34,6 +36,7 @@ contract NftCertificate is ERC721, Ownable {
         tokenURIs[tokenId] = _tokenURI; // Simpan hash IPFS metadata
     }
 
+    //override tokenURI from NFT metadata
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return tokenURIs[tokenId];
     }
